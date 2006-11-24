a6d12ac9882c269a130d8ab860f49ada4f198a2b
carsten.kolve <carsten.kolve@db152395-6821-0410-9a54-57c07494a053>
2006-11-24 06:51:25 +0000

    added more display options to puttyGlyph node

--- include/puttyGlyphNode.h
+++ include/puttyGlyphNode.h
@@ -23,8 +23,12 @@
 
 
 
-#define PG_GT_BBOX 1
-#define PG_GT_VECTOR 2
+#define PG_GT_BBOX 	 3
+#define PG_GT_LINE 	 2
+#define PG_GT_VECTOR 1
+
+
+
 
 class puttyGlyph : public MPxLocatorNode
 {
@@ -35,14 +39,18 @@ class puttyGlyph : public MPxLocatorNode
 		static	MStatus	initialize();
         
 		virtual	void	postConstructor();            
-           
+        
+		virtual void drawBBox(MVectorArray &vecIn1,MVectorArray &vecIn2);
+		virtual void drawLine(MVectorArray &vecIn1,MVectorArray &vecIn2);
+		virtual void drawVector(MVectorArray &vecIn1,MVectorArray &vecIn2,float scale);
+                        
         virtual void draw( M3dView & view, const MDagPath & path, 
 		        		   M3dView::DisplayStyle style,
 						   M3dView::DisplayStatus status );
 
        	virtual bool isBounded() const;
 		virtual MBoundingBox boundingBox() const;
-       	virtual bool isTransparent() const; 
+//     	virtual bool isTransparent() const; // maya 8
        
 	
 		static  MTypeId id;
@@ -50,6 +58,27 @@ class puttyGlyph : public MPxLocatorNode
         static  MObject aGlyphType; // type
         static  MObject aVecInput1; // first vector array
         static  MObject aVecInput2; // 2nd vector array        
+
+
+	    static MObject aLineWidth;
+	    static MObject aPointSize;        
+
+	    static MObject aDrawLines;
+	    static MObject aDrawPoint1;        
+	    static MObject aDrawPoint2;                
+        
+	        static MObject aLineColorR; 
+    	    static MObject aLineColorG; 
+	        static MObject aLineColorB; 
+        static MObject aLineColor; 
+  
+  	        static MObject aPointColorR; 
+    	    static MObject aPointColorG; 
+	        static MObject aPointColorB; 
+        static MObject aPointColor; 
+  
+  
+        static  MObject aScale; // scale factor
         
 	private:
     	
--- src/puttyGlyphNode.cpp
+++ src/puttyGlyphNode.cpp
@@ -38,7 +38,28 @@ MTypeId puttyGlyph::id( PUTTYGLYPH_NODE_ID );
      
 MObject puttyGlyph::aVecInput1;   
 MObject puttyGlyph::aVecInput2;   
-MObject puttyGlyph::aGlyphType;   
+
+MObject puttyGlyph::aGlyphType;
+MObject puttyGlyph::aScale; 
+
+MObject puttyGlyph::aLineWidth; 
+MObject puttyGlyph::aPointSize; 
+MObject puttyGlyph::aDrawLines; 
+MObject puttyGlyph::aDrawPoint1; 
+MObject puttyGlyph::aDrawPoint2; 
+
+MObject puttyGlyph::aLineColorR; 
+MObject puttyGlyph::aLineColorG; 
+MObject puttyGlyph::aLineColorB; 
+MObject puttyGlyph::aLineColor; 
+
+MObject puttyGlyph::aPointColorR; 
+MObject puttyGlyph::aPointColorG; 
+MObject puttyGlyph::aPointColorB; 
+MObject puttyGlyph::aPointColor; 
+    
+
+
 
 /*************************************************************************************/
 // constructor, build all display lists required
@@ -100,18 +121,88 @@ void* puttyGlyph::creator()
 MStatus puttyGlyph::initialize()
 {
 	MStatus status;
-    MFnTypedAttribute  tAttr;
-    MFnEnumAttribute   eAttr;    
+    MFnTypedAttribute  	tAttr;
+    MFnEnumAttribute   	eAttr;    
+    MFnNumericAttribute nAttr;        
 
     // the particle object
     
-    // def space
-   	aGlyphType = eAttr.create("glyphType", "gt", PG_GT_BBOX, &status);
-	eAttr.addField("bounding box",PG_GT_BBOX);
+    // type
+   	aGlyphType = eAttr.create("glyphType", "gt", PG_GT_VECTOR, &status);
+	eAttr.addField("vector (v1 pos, v2 dir)",PG_GT_VECTOR);        
+	eAttr.addField("line (v1, v2 pos)",PG_GT_LINE);    
+	eAttr.addField("bounding box (v1, v2 corner)",PG_GT_BBOX);
    	eAttr.setKeyable(true);
 	eAttr.setStorable(true);
 	SYS_ERROR_CHECK( addAttribute( aGlyphType ), "adding aGlyphType" );
     
+	aScale =  nAttr.create( "scale", "sc", MFnNumericData::kFloat );
+	nAttr.setDefault( 1.0 ); 
+   	nAttr.setSoftMin( -5.0 ); 
+   	nAttr.setSoftMax( 5.0 ); 
+	nAttr.setKeyable( true ); 
+	nAttr.setReadable( true ); 
+	nAttr.setWritable( true ); 
+	nAttr.setStorable( true ); 
+    SYS_ERROR_CHECK( addAttribute( aScale ), "adding aScale" );   
+    
+
+
+	// display options
+
+
+    aDrawLines = nAttr.create("drawLines", "dl", MFnNumericData::kBoolean, true);
+    SYS_ERROR_CHECK( addAttribute( aDrawLines ), "adding aDrawLines" ); 
+
+    aDrawPoint1 = nAttr.create("drawPoint1", "dp1", MFnNumericData::kBoolean, true);
+    SYS_ERROR_CHECK( addAttribute( aDrawPoint1 ), "adding aDrawPoint1" ); 
+
+    aDrawPoint2 = nAttr.create("drawPoint2", "dp2", MFnNumericData::kBoolean, true);
+    SYS_ERROR_CHECK( addAttribute( aDrawPoint2 ), "adding aDrawPoint2" ); 
+    
+    
+    	aLineColorR = nAttr.create( "lineColorR", "lcolr", MFnNumericData::kFloat );
+        aLineColorG = nAttr.create( "lineColorG", "lcolg", MFnNumericData::kFloat );
+        aLineColorB = nAttr.create( "lineColorB", "lcolb", MFnNumericData::kFloat );        
+	aLineColor = nAttr.create( "lineColor", "lcol", aLineColorR, aLineColorG, aLineColorB);
+    nAttr.setDefault(1.0,0.0,0.0);
+ 	nAttr.setStorable( true );
+ 	nAttr.setUsedAsColor( true );
+    SYS_ERROR_CHECK ( addAttribute( aLineColor ), "adding aLineColor" );   
+
+
+    	aPointColorR = nAttr.create( "pointColorR", "pcolr", MFnNumericData::kFloat);
+        aPointColorG = nAttr.create( "pointColorG", "pcolg", MFnNumericData::kFloat);
+        aPointColorB = nAttr.create( "pointColorB", "pcolb", MFnNumericData::kFloat);        
+	aPointColor = nAttr.create( "pointColor", "pcol", aPointColorR, aPointColorG, aPointColorB);
+    nAttr.setDefault(1.0,1.0,1.0);
+ 	nAttr.setStorable( true );
+ 	nAttr.setUsedAsColor( true );
+    SYS_ERROR_CHECK ( addAttribute( aPointColor ), "adding aPointColor" );   
+
+	aLineWidth =  nAttr.create( "lineWidth", "lw", MFnNumericData::kFloat );
+	nAttr.setDefault( 1.0 ); 
+   	nAttr.setMin( 0.0 ); 
+   	nAttr.setSoftMax( 5.0 ); 
+	nAttr.setKeyable( true ); 
+	nAttr.setReadable( true ); 
+	nAttr.setWritable( true ); 
+	nAttr.setStorable( true ); 
+    SYS_ERROR_CHECK( addAttribute( aLineWidth ), "adding aLineWidth" );   
+    
+	aPointSize =  nAttr.create( "pointSize", "ps", MFnNumericData::kFloat );
+	nAttr.setDefault( 1.0 ); 
+   	nAttr.setMin( 0.0 ); 
+   	nAttr.setSoftMax( 5.0 ); 
+	nAttr.setKeyable( true ); 
+	nAttr.setReadable( true ); 
+	nAttr.setWritable( true ); 
+	nAttr.setStorable( true ); 
+    SYS_ERROR_CHECK( addAttribute( aPointSize ), "adding aPointSize" );   
+    
+    
+
+    // inputs
     aVecInput1 = tAttr.create( "vecInput1", "vi1", MFnData::kVectorArray);
     tAttr.setStorable(false);
     tAttr.setKeyable(false);
@@ -179,8 +270,65 @@ MBoundingBox puttyGlyph::boundingBox() const
 }
 
 /*************************************************************************************/
-// transparency
-bool puttyGlyph::isTransparent( ) const { return false; }
+// transparency, maya 8
+//bool puttyGlyph::isTransparent( ) const { return false; }
+
+
+/*************************************************************************************/
+// draw bounding box
+void puttyGlyph::drawBBox(MVectorArray &vecIn1,MVectorArray &vecIn2)
+{
+	for (int i=0;i<vecIn1.length();i++)
+    {
+        glPushMatrix();
+        
+        // move to the correct spot, set up the right scale
+        MVector diff = vecIn2[i] - vecIn1[i];        
+        MVector pos = (vecIn1[i] + vecIn2[i]) * 0.5;
+        
+        glTranslatef(float(pos.x),float(pos.y),float(pos.z));            
+        glScalef(fabs(diff.x),fabs(diff.y),fabs(diff.z));                
+
+        // call the list
+        glCallList(mBBoxDL);
+        glPopMatrix();
+    }        
+}
+
+/*************************************************************************************/
+// draw line
+void puttyGlyph::drawLine(MVectorArray &vecIn1,MVectorArray &vecIn2)
+{
+	glBegin(GL_LINES);
+    
+	for (int i=0;i<vecIn1.length();i++)
+    {
+		glVertex3f(vecIn1[i].x,vecIn1[i].y,vecIn1[i].z);
+		glVertex3f(vecIn2[i].x,vecIn2[i].y,vecIn2[i].z);        
+    }        
+    
+    glEnd();
+}
+
+/*************************************************************************************/
+// draw vector
+void puttyGlyph::drawVector(MVectorArray &vecIn1,MVectorArray &vecIn2, float scale)
+{
+	glBegin(GL_LINES);
+    
+	for (int i=0;i<vecIn1.length();i++)
+    {
+		glVertex3f(vecIn1[i].x,vecIn1[i].y,vecIn1[i].z);
+
+        MVector pos2 = vecIn1[i] + scale * vecIn2[i];
+        
+		glVertex3f(pos2.x,pos2.y,pos2.z);        
+    }        
+    
+    glEnd();
+}
+
+
 
 /*************************************************************************************/
 // draw
@@ -213,6 +361,48 @@ void puttyGlyph::draw( M3dView & view, const MDagPath & /*path*/,
     MFnVectorArrayData vecFn2(vecObj);
     vecIn2 = vecFn2.array(&status);
     
+    float scale;
+    plug.setAttribute(aScale);
+    plug.getValue(scale);
+    
+    ///////////////////////////////////////////////////////////
+    // drawing parameters
+    
+    plug.setAttribute(aDrawLines);
+    bool mDrawLines; plug.getValue(mDrawLines);
+      
+    plug.setAttribute(aDrawPoint1);
+    bool mDrawPoint1; plug.getValue(mDrawPoint1);
+
+    plug.setAttribute(aDrawPoint2);
+    bool mDrawPoint2; plug.getValue(mDrawPoint2);
+    
+    plug.setAttribute(aPointSize);
+    float mPointSize; plug.getValue(mPointSize);
+
+    plug.setAttribute(aLineWidth);
+    float mLineWidth; plug.getValue(mLineWidth);
+
+    // color
+    plug.setAttribute(aLineColorR);
+    float mLineColorR; plug.getValue(mLineColorR);
+
+    plug.setAttribute(aLineColorG);
+    float mLineColorG; plug.getValue(mLineColorG);
+
+    plug.setAttribute(aLineColorB);
+    float mLineColorB; plug.getValue(mLineColorB);
+    
+    plug.setAttribute(aPointColorR);
+    float mPointColorR; plug.getValue(mPointColorR);
+
+    plug.setAttribute(aPointColorG);
+    float mPointColorG; plug.getValue(mPointColorG);
+
+    plug.setAttribute(aPointColorB);
+    float mPointColorB; plug.getValue(mPointColorB);    
+      
+    
     /////////////////////////////////////////////////////////////////////
     // verify the values
     
@@ -224,39 +414,57 @@ void puttyGlyph::draw( M3dView & view, const MDagPath & /*path*/,
 	
 	view.beginGL();             
 	
-    
-    if (type == PG_GT_BBOX)
+
+    glPushAttrib( GL_CURRENT_BIT );
+    glMatrixMode (GL_MODELVIEW);
+
+	// draw the lines
+	if (mDrawLines)
     {
-        glPushAttrib( GL_CURRENT_BIT );
-        glMatrixMode (GL_MODELVIEW);
-        
-        // color    
-		if ( status == M3dView::kActive ) 
-			view.setDrawColor( 13, M3dView::kActiveColors );
-		else
-			view.setDrawColor( 13, M3dView::kDormantColors );
+    
+        float lw;
+        glGetFloatv(GL_LINE_WIDTH,&lw);
+	    glLineWidth(mLineWidth);
 
-	    for (int i=0;i<vecIn1.length();i++)
+	   	glColor3f(mLineColorR,mLineColorG,mLineColorB);
+    
+	    switch(type)
     	{
-            glPushMatrix();
-            
-            // move to the correct spot, set up the right scale
-            MVector diff = vecIn2[i] - vecIn1[i];        
-            MVector pos = (vecIn1[i] + vecIn2[i]) * 0.5;
-            
-            glTranslatef(float(pos.x),float(pos.y),float(pos.z));            
-	        glScalef(fabs(diff.x),fabs(diff.y),fabs(diff.z));                
-
-            // call the list
-            glCallList(mBBoxDL);
-            
-            glPopMatrix();
-	    }        
+    		case (PG_GT_BBOX): { drawBBox(vecIn1,vecIn2); break; }
+	    	case (PG_GT_LINE): { drawLine(vecIn1,vecIn2); break; }
+    		case (PG_GT_VECTOR): { drawVector(vecIn1,vecIn2,scale); break; }        
+	    }
         
-    	glPopAttrib();
+       	glLineWidth(lw);
     }
-	
+    
+    
+    
+	// draw the points
+    glColor3f(mPointColorR,mPointColorG,mPointColorB);
+    
+    float ps;
+    glGetFloatv(GL_POINT_SIZE,&ps); 
+//    glPointSize(mPointSize);
 
+	glBegin (GL_POINTS);
+
+	if (mDrawPoint1)
+		for (int i=0;i<vecIn1.length();i++)
+        	glVertex3f(vecIn1[i].x,vecIn1[i].y,vecIn1[i].z);
+            
+	if (mDrawPoint2)
+		for (int i=0;i<vecIn2.length();i++)
+        	glVertex3f(vecIn2[i].x,vecIn2[i].y,vecIn2[i].z);
+
+	glEnd();
+    
+//    glPointSize(ps);
+	glPopAttrib();
+    
+    
+    // done
+    
 	view.endGL();
     
 
